a6a4a8f8596a60accec249ff540c0399e00b5959
calvinraab <calvin.raab@gmail.com>
2022-09-27 16:16:15 -0700

    Up and running with user data
    This code creates a place for Users to be created.

--- notes.md
+++ notes.md
@@ -2,3 +2,21 @@ Notes:
 
 - remember to do just 'python' when on windows
 - In the installed apps the 'auth' related stuff is very important
+
+superuser username: calvin
+Password: calvin
+
+user: joe
+Password: joe (maybe)
+
+user: adam
+password: thisisadamspassword123!
+
+User: Test
+Password: Letstestthispassword!
+
+To figure out:
+
+- Whenever I am creating a user currently it appears to save thier password as their email as well? Not sure why that is. It doesn't appear to be doing that in the video.
+
+Video I left off on: 63
--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+# Django forms is  whole topic within itself
+from django.contrib.auth.forms import UserCreationForm
+# Helps us create user objects really quickly
+# This is  the user model we will be using
+from django.contrib.auth.models import User
 
 # Create your views here.
+
+
+def signupuser(request):
+    if request.method == "GET":
+        return render(request, "todo/signupuser.html", {'form': UserCreationForm()})
+    else:  # We know it is a post
+        if request.POST['password1'] == request.POST['password2']:
+            # Create a new user
+            user = User.objects.create_user(
+                request.POST['username'], request.POST['password1'])
+            user.save()  # This inserts it into the database
+        else:
+            print('Hello')
+            # Tell the user that the passwords did nto match
