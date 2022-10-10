97cfa4080dc589a5169d858244907a13c9e26c67
Calvin Raab - SANDAG <calvin.raab@gmail.com>
2022-10-09 22:22:14 -0700

    Debugging sign up steps

--- todo/views.py
+++ todo/views.py
@@ -1,9 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 # Django forms is  whole topic within itself
 from django.contrib.auth.forms import UserCreationForm
 # Helps us create user objects really quickly
 # This is  the user model we will be using
 from django.contrib.auth.models import User
+from django.db import IntegrityError
+from django.contrib.auth import login
 
 # Create your views here.
 
@@ -16,10 +18,22 @@ def signupuser(request):
         return render(request, "todo/signupuser.html", {'form': UserCreationForm()})
     else:  # We know it is a post
         if request.POST['password1'] == request.POST['password2']:
-            # Create a new user
-            user = User.objects.create_user(
-                username=request.POST['username'], password=request.POST['password1'])
-            user.save()  # This inserts it into the database
+            try:
+                # Create a new user
+                user = User.objects.create_user(
+                    username=request.POST['username'], password=request.POST['password1'])
+                user.save()  # This inserts it into the database
+            # This is the error you get if a username already exists that you are trying.
+                # After creating an account this keeps the user logged in.
+                login(request, user)
+                return redirect('currenttodos')
+            except IntegrityError:
+                return render(request, "todo/signupuser.html", {'form': UserCreationForm(), 'error': 'That username has already been taken. Please choose a new username.'})
+
         else:
-            print('Hello')
+            return render(request, "todo/signupuser.html", {'form': UserCreationForm(), 'error': 'Passwords did not match'})
             # Tell the user that the passwords did not match
+
+
+def currenttodos(request):
+    return render(request, 'todo/currenttodos.html')
