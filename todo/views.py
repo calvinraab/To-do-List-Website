5911f0cb15eee770133844509143b14ae454cb9e
Calvin Raab - SANDAG <calvin.raab@gmail.com>
2022-11-01 22:04:49 -0700

    Logout build

--- todo/views.py
+++ todo/views.py
@@ -5,11 +5,15 @@ from django.contrib.auth.forms import UserCreationForm
 # This is  the user model we will be using
 from django.contrib.auth.models import User
 from django.db import IntegrityError
-from django.contrib.auth import login
+from django.contrib.auth import login, logout
 
 # Create your views here.
 
 ''' potential fix: https://stackoverflow.com/questions/48448563/my-password-is-stored-inside-the-email-field-in-django-admin'''
+
+
+def home(request):
+    return render(request, "todo/home.html")
 
 
 def signupuser(request):
@@ -34,6 +38,12 @@ def signupuser(request):
         else:
             return render(request, "todo/signupuser.html", {'form': UserCreationForm(), 'error': 'Passwords did not match'})
             # Tell the user that the passwords did not match
+
+
+def logoutuser(request):
+    if request.method == 'POST':  # We only want to log people out if it is a post, default in html is going to be a GET that would log us out when we open teh page because Chrome starts running all of teh GET functions
+        logout(request)
+        return redirect('home')
 
 
 def currenttodos(request):
