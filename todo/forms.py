1a96eca012bf25cd9a7de1cdf156f0f84c6baa93
Calvin Raab - SANDAG <calvin.raab@gmail.com>
2022-11-13 22:51:34 -0800

    Form notes update

--- todo/forms.py
+++ todo/forms.py
@@ -5,7 +5,7 @@ from .models import Todo
 # User creation and authentication form were forms by Django, I am now going to make my own form
 
 
-class Todoform(ModelForm):  # This is going to inherit from ModelForm
+class Todoform(ModelForm):  # This inherits from ModelForm
     class Meta:  # I think that is a class from Django?
         model = Todo
         fields = ['title', 'memo', 'important']
